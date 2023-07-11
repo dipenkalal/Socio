7ea968770a0cfb5ab6b6fdad6035e2d03e8b12e9
Dipen Kalal <57854287+dipenkalal@users.noreply.github.com>
2023-07-11 16:08:58 -0400

    major update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import RegisterPage from "scenes/registerPage";
+import CarpoolingPage from "scenes/carpoolingPage";
+import EVStationPage from "scenes/evstationPage";
+import WinsdorTransitPage from "scenes/windsortransitPage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import WelcomePage from "scenes/welcomePage";
+import DashboardPage from "scenes/dashboardPage/dashborad";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  // const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+          <Route path="/" element={<HomePage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/carpooling" element={<CarpoolingPage />} />
+            <Route path="/evstation" element={<EVStationPage />} />
+            <Route path="/windsortransit" element={<WinsdorTransitPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            {/* <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <HomePage />}
+            /> */}
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
